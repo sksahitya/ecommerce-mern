c38cd622aa3ff9757236654e2c80c13458bc629a
sksahitya <sahitya.shweta@gmail.com>
2024-10-29 20:18:40 -0400

    changed cookie logic to session storage for render

--- client/src/components/shop/header.jsx
+++ client/src/components/shop/header.jsx
@@ -6,7 +6,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { shoppingViewHeaderMenuItems } from "@/config";
 import {  DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger,} from "../ui/dropdown-menu";
 import { Avatar, AvatarFallback } from "../ui/avatar";
-import { logoutUser } from "@/store/auth-slice";
+import { logoutUser, resetTokenAndCredentials } from "@/store/auth-slice";
 import UserCartWrapper from "./cart-wrapper";
 import { useEffect, useState } from "react";
 import { fetchCartItems } from "@/store/shop/cart-slice";
@@ -68,7 +68,10 @@ function HeaderRightContent() {
   const dispatch = useDispatch();
 
   function handleLogout() {
-    dispatch(logoutUser());
+    // dispatch(logoutUser());
+    dispatch(resetTokenAndCredentials())
+    sessionStorage.clear()
+    navigate("/auth/login");
   }
 
   useEffect(() => {
--- client/src/store/auth-slice/index.js
+++ client/src/store/auth-slice/index.js
@@ -5,6 +5,7 @@ const initialState = {
   isAuthenticated: false,
   isLoading: true,
   user: null,
+  token: null,
 };
 
 export const registerUser = createAsyncThunk(
@@ -58,29 +59,53 @@ export const logoutUser = createAsyncThunk(
   }
 );
 
+// export const checkAuth = createAsyncThunk(
+//   "/auth/checkauth",
+//   async () => {
+//     const response = await axios.get(
+//       `${import.meta.env.VITE_API_URL}/api/auth/check-auth`,
+//       {
+//         withCredentials: true,
+//         headers: {
+//           "Cache-Control":
+//             "no-store, no-cache, must-revalidate, proxy-revalidate",
+//         },
+//       }
+//     );
+
+//     return response.data;
+//   }
+// );
+
 export const checkAuth = createAsyncThunk(
   "/auth/checkauth",
-  async () => {
+  async (token) => {
     const response = await axios.get(
       `${import.meta.env.VITE_API_URL}/api/auth/check-auth`,
       {
-        withCredentials: true,
         headers: {
+          Authorization: `Bearer ${token}`,
           "Cache-Control":
             "no-store, no-cache, must-revalidate, proxy-revalidate",
         },
+        withCredentials: true,
       }
     );
-
     return response.data;
   }
 );
+
 
 const authSlice = createSlice({
   name: "auth",
   initialState,
   reducers: {
     setUser: (state, action) => {},
+    resetTokenAndCredentials: (state) => {
+      state.isAuthenticated = false,
+      state.token = null, 
+      state.user = null
+    }
   },
   extraReducers: (builder) => {
     builder
@@ -105,6 +130,8 @@ const authSlice = createSlice({
         state.isLoading = false;
         state.user = action.payload.success ? action.payload.user : null;
         state.isAuthenticated = action.payload.success;
+        state.token = action.payload.token;
+        sessionStorage.setItem("token", JSON.stringify(action.payload.token))
 
         if (action.payload.success) {
           localStorage.removeItem("guestId");
@@ -114,6 +141,7 @@ const authSlice = createSlice({
         state.isLoading = false;
         state.user = null;
         state.isAuthenticated = false;
+        state.token = null;
       })
       .addCase(checkAuth.pending, (state) => {
         state.isLoading = true;
@@ -136,5 +164,5 @@ const authSlice = createSlice({
   },
 });
 
-export const { setUser } = authSlice.actions;
+export const { setUser, resetTokenAndCredentials } = authSlice.actions;
 export default authSlice.reducer;
